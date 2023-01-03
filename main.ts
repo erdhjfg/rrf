3d2f2ad5be13abc9d8e15beb95d2824910e31588
erdhjfg <121865822+erdhjfg@users.noreply.github.com>
2023-01-03 19:37:31 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    turtle.setPosition(2, 2)
+    turtle.pen(TurtlePenMode.Down)
+    for (let index = 0; index <= 4; index++) {
+        turtle.forward(1)
+        turtle.turnRight()
+    }
+    turtle.pen(TurtlePenMode.Up)
+    turtle.home()
+    basic.showString(" ")
+})
